c946b794baeb847f413301e700818687f968f5c2
arhimason <tqp@ya.ru>
2019-10-01 21:18:07 +0400

    fix example urls

--- bot_handler/configurations/ExampleBot/urls.py
+++ bot_handler/configurations/ExampleBot/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from bot_handler import handler
+from bot_handler.core.messengers import VKMessenger, TGMessenger
 
+vk_messenger = VKMessenger()
+tg_messenger = TGMessenger()
 urlpatterns = [
 
-    url(r'^telegram$', handler.tg_request_receiver, name='tg_request_receiver'),
-    url(r'^vkGroup$', handler.vk_request_receiver, name='vk_request_receiver'),
-
+    url(r'^telegram$', tg_messenger.handle_request, name='tg_request_receiver'),
+    url(r'^vkGroup$', vk_messenger.handle_request, name='vk_request_receiver'),
 ]
